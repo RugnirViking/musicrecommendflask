89eb6869054f1ffcd6251d90b7ce3be893b51220
Samuel Howell <rugnirtheviking@gmail.com>
2022-11-12 11:33:57 +0100

    more

---
+++ flaskr/static/script.js
@@ -0,0 +1,41 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // GDP data
+    var svgMapDataGPD = {
+        data: {
+          gdp: {
+            name: 'GDP per capita',
+            format: '{0} USD',
+            thousandSeparator: ',',
+            thresholdMax: 50000,
+            thresholdMin: 1000
+          },
+          change: {
+            name: 'Change to year before',
+            format: '{0} %'
+          },
+          gdpAdjusted: {
+            name: 'Purchasing Power Parity',
+            format: '{0} USD',
+            thousandSeparator: ',',
+            thresholdMax: 50000,
+            thresholdMin: 1000
+          },
+          changeAdjusted: {
+            name: 'Change to year before',
+            format: '{0} %'
+          }
+        },
+        applyData: 'gdpAdjusted',
+        values: {
+          AF: {gdp: 587, change: 4.73, gdpAdjusted: 1958, changeAdjusted: 0.02, link: 'https://cssscript.com'}
+        }
+    }
+
+    new svgMap({
+        targetElementID: 'svgMapExample',
+        data: svgMapDataGPD,
+        touchLink: true
+    });
+}, false);
